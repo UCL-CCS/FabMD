c93a695e1296f07b3fce7be6c6e4d2b85f352360
Derek Groen <djgroennl@gmail.com>
2019-09-24 15:20:43 +0100

    Added restarting script.

--- lammps_restart.py
+++ lammps_restart.py
@@ -6,31 +6,18 @@ def lammps_restart(config, results_dir, **args):
     defaults = yaml.load(open(FabMD_path+'/default_settings/lammps.yaml'))
     update_environment(defaults)
     update_environment(args)
-    #update_environment(dict(lammps_input="restart.{}".format(env.lammps_input)))
 
-    if "_restart_" in results_dir:
+    job_number = 1
+    if "restart_" in results_dir:
         results_chunks = results_dir.split('_')
-        job_number = int(results_chunks[-1])
+        job_number = int(results_chunks[1]) #  results_dir starts with 'restart_<x>'.
         job_number += 1
-        results_chunks[-1] = str(job_number)
-        new_results_dir = '_'.join(results_chunks)
-        update_environment({'job_results': new_results_dir, 'job_results_contents_local', new_results_dir})
+
+    if hasattr(env, "label"):
+        env.label += "_restart_{}".format(job_number)
     else:
-        update_environment({'job_results': results_dir + '_restart_1'})
-        update_environment({'job_results_contents_local', results_dir + '_restart_1'})
+        env.label = "restart_{}".format(job_number)
 
-    md_job(config, 'lammps', lammps_input="restart.{}".format(env.lammps_input), job_results=env.job_results, job_results_contents_local=env.job_results_contents_local, **args)
-
+    md_job(config, 'lammps', lammps_input="restart.{}".format(env.lammps_input), label=env.label, **args)
 
 
-    """
-    with_config(config)
-    execute(put_configs, config)
-    defaults = yaml.load(open(FabMD_path+'/default_settings/'+script+'.yaml'))
-    job(dict(script=script,
-             wall_time=defaults['wall_time'],
-             lammps_input=defaults['lammps_input'],
-             cores=defaults['cores'],
-             memory='2G'),
-        args)
-    """
