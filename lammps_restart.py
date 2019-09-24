097ef78d1a3028099c0cb1876979084a3e84da71
Derek Groen <djgroennl@gmail.com>
2019-09-24 15:01:29 +0100

    Initial restart incrementor.

--- lammps_restart.py
+++ lammps_restart.py
@@ -2,12 +2,26 @@ from base.fab import *
 from plugins.FabMD.FabMD import *
 
 @task
-def lammps_restart(config, **args):
+def lammps_restart(config, results_dir, **args):
     defaults = yaml.load(open(FabMD_path+'/default_settings/lammps.yaml'))
     update_environment(defaults)
     update_environment(args)
     #update_environment(dict(lammps_input="restart.{}".format(env.lammps_input)))
-    md_job(config, 'lammps', lammps_input="restart.{}".format(env.lammps_input), **args)
+
+    if "_restart_" in results_dir:
+        results_chunks = results_dir.split('_')
+        job_number = int(results_chunks[-1])
+        job_number += 1
+        results_chunks[-1] = str(job_number)
+        new_results_dir = '_'.join(results_chunks)
+        update_environment({'job_results': new_results_dir, 'job_results_contents_local', new_results_dir})
+    else:
+        update_environment({'job_results': results_dir + '_restart_1'})
+        update_environment({'job_results_contents_local', results_dir + '_restart_1'})
+
+    md_job(config, 'lammps', lammps_input="restart.{}".format(env.lammps_input), job_results=env.job_results, job_results_contents_local=env.job_results_contents_local, **args)
+
+
 
     """
     with_config(config)
